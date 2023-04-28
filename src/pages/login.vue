bae745c3f2b30941c673f473970fc6f8fa44f21f
LHD <2293725360@qq.com>
2023-04-29 01:29:48 +0800

    feat:完成登录页页面展示

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,160 +1,118 @@
 <script setup lang="ts">
-import anime from 'animejs';
-// 定义一个anime对象
-let an: anime.AnimeInstance;
-const animeConfig = [
-  {
-    id: '#account',
-    type: 'click',
-    config: {
-      targets: 'path',
-      strokeDashoffset: {
-        value: 0,
-        duration: 700,
-        easing: 'easeOutQuart'
-      },
-      strokeDasharray: {
-        value: '240 1386',
-        duration: 700,
-        easing: 'easeOutQuart'
-      }
-    }
-  },
-  {
-    id: '#password',
-    type: 'click',
-    config: {
-      targets: 'path',
-      strokeDashoffset: {
-        value: -336,
-        duration: 700,
-        easing: 'easeOutQuart'
-      },
-      strokeDasharray: {
-        value: '240 1386',
-        duration: 700,
-        easing: 'easeOutQuart'
-      }
-    }
-  },
-  {
-    id: '#submit',
-    type: 'mouseover',
-    config: {
-      targets: 'path',
-      strokeDashoffset: {
-        value: -730,
-        duration: 700,
-        easing: 'easeOutQuart'
-      },
-      strokeDasharray: {
-        value: '530 1386',
-        duration: 700,
-        easing: 'easeOutQuart'
-      }
-    }
-  },
-  {
-    id: '#account',
-    type: 'focus',
-    config: {
-      targets: 'path',
-      strokeDashoffset: {
-        value: 0,
-        duration: 700,
-        easing: 'easeOutQuart'
-      },
-      strokeDasharray: {
-        value: '240 1386',
-        duration: 700,
-        easing: 'easeOutQuart'
-      }
-    }
-  },
-  {
-    id: '#password',
-    type: 'focus',
-    config: {
-      targets: 'path',
-      strokeDashoffset: {
-        value: -336,
-        duration: 700,
-        easing: 'easeOutQuart'
-      },
-      strokeDasharray: {
-        value: '240 1386',
-        duration: 700,
-        easing: 'easeOutQuart'
-      }
-    }
-  },
-  {
-    id: '#submit',
-    type: 'focus',
-    config: {
-      targets: 'path',
-      strokeDashoffset: {
-        value: -730,
-        duration: 700,
-        easing: 'easeOutQuart'
-      },
-      strokeDasharray: {
-        value: '530 1386',
-        duration: 700,
-        easing: 'easeOutQuart'
-      }
-    }
-  }
-]
-
+import $ from 'jquery'
+let isIn = true
+let isOut = false
+let span: HTMLElement
+let con = ref()
+let codeText = ref<any>('获取验证码')
+let timer: number[] = []
+let account = ref('')
+let password = ref('')
+let code = ref('')
 onMounted(() => {
-  init();
+  init()
 })
 
-// 绑定动画事件
-const init = () => {
-  // 绑定账号输入框的点击事件
-  animeConfig.forEach(item => {
-    document.querySelector(item.id)!.addEventListener(item.type, function () {
-      // 如果已存在anime动画,先暂停正在运行的动画
-      if (an) an.pause();
-      // 构造一个动画对象
-      an = anime(item.config);
-    });
-  })
+const getCode = () => {
+  if ((typeof codeText.value) === 'number') {
+    // 正在获取
+    return
+  }
+  codeText.value = 60
+  timer.push(window.setInterval(() => {
+    if (codeText.value >= 1) {
+      codeText.value = codeText.value - 1
+    } else {
+      codeText.value = '重新获取'
+    }
+  }, 1000))
 }
+
+const login = () => {
+  con.value.classList.remove('fail')
+  con.value.classList.add('success');
+  console.log(account.value)
+  console.log(password.value)
+  console.log(code.value)
+  setTimeout(() => {
+    con.value.classList.add('fail')
+  }, 2000);
+}
+
+const init = () => {
+  con.value.onmouseenter = (e: any) => {
+    // 如果进去的门是打开的，就可以执行这个函数
+    if (isIn) {
+      // 获取进入的鼠标位置
+      // 生成元素的位置=进入点距离窗口的距离-父盒子距离窗口的距离
+      let inX = e.clientX - e.target.offsetLeft;
+      let inY = e.clientY - e.target.offsetTop;
+
+      // 创建一个span元素，并且给它对应的出生坐标
+      let el = document.createElement('span');
+      el.style.left = inX + 'px';
+      el.style.top = inY + 'px';
+      // 添加到con对应的父元素，即container
+      con.value.appendChild(el);
+
+      $('.container span').removeClass('out');    // 移除出去的动画
+      $('.container span').addClass('in');        // 添加进入的动画
+      span = document.querySelector('.container span')!;
+      isIn = false;     // 关闭进来的门（不能使用进入的方法）
+      isOut = true;     // 打开出去的门（可以使用出去的方法）
+    }
+  }
+  // 监听鼠标出去的事件
+  con.value.onmouseleave = (e: any) => {
+    if (isOut) {
+      // 获取出去的鼠标位置
+      // 生成元素的位置=出去点距离窗口的距离-父盒子距离窗口的距离
+      let outX = e.clientX - e.target.offsetLeft;
+      let outY = e.clientY - e.target.offsetTop;
+
+      $('.container span').removeClass('in');     // 移除进入的动画
+      $('.container span').addClass('out');       // 添加出去的动画
+      // 添加出去的坐标
+      $('.out').css('left', outX + 'px');
+      $('.out').css('top', outY + 'px');
+
+      isOut = false;    // 关闭出去的门
+      // 当动画结束后再删除元素
+      timer.push(window.setTimeout(() => {
+        con.value.removeChild(span);      // 删除元素
+        isIn = true;                  // 打开进入的门
+      }, 500))
+    }
+  }
+}
+
+onBeforeUnmount(() => {
+  timer.forEach(i => {
+    window.clearTimeout(i)
+    window.clearInterval(i)
+  })
+  con.value.onmouseenter = null
+  con.value.onmouseleave = null
+})
 </script>
 
 <template>
   <div class="main">
-    <div class="container">
-      <div class="left">
-        <h1>login</h1>
-        <p>欢迎光临, 请登录!</p>
-      </div>
-      <div class="right">
-        <!-- SVG提前准备好, 来自互联网 -->
-        <svg viewBox="0 0 320 300">
-          <defs>
-            <!-- 定义线性渐变 -->
-            <linearGradient inkscape:collect="always" id="linearGradient1" x1="13" y1="193.49992" x2="307" y2="193.49992"
-              gradientUnits="userSpaceOnUse">
-              <stop style="stop-color:#0ff;" offset="0" />
-              <stop style="stop-color:#f0f;" offset="1" />
-            </linearGradient>
-          </defs>
-          <path
-            d="m 40,120.00016 239.99984,-3.2e-4 c 0,0 24.99263,0.79932 25.00016,35.00016 0.008,34.20084 -25.00016,35 -25.00016,35 h -239.99984 c 0,-0.0205 -25,4.01348 -25,38.5 0,34.48652 25,38.5 25,38.5 h 215 c 0,0 20,-0.99604 20,-25 0,-24.00396 -20,-25 -20,-25 h -190 c 0,0 -20,1.71033 -20,25 0,24.00396 20,25 20,25 h 168.57143" />
-        </svg>
-        <div class="form">
-          <label for="account" class="account">账号</label>
-          <input type="text" id="account" autocomplete="off">
-          <label for="password" class="password">密码</label>
-          <input type="password" id="password">
-          <button id="submit">登录</button>
+    <div class="container" ref="con">
+      <h1>CarLoans</h1>
+      <div class="form">
+        <input type="text" placeholder="您的账号" v-model="account">
+        <input type="password" placeholder="您的密码" v-model="password">
+        <div class="code">
+          <input type="text" placeholder="验证码" v-model="code">
+          <div class="num" @click="getCode">{{ (typeof codeText) === 'number' ? codeText + ' s' : codeText }}</div>
         </div>
+        <button class="btn-login" @click="login">登录</button>
       </div>
     </div>
+    <ul class="bg-squares">
+      <li v-for="item in 10" :key="item"></li>
+    </ul>
   </div>
 </template>
 
@@ -162,114 +120,311 @@ const init = () => {
 .main {
   height: 100vh;
   width: 100vw;
-  /* 弹性布局 居中显示 */
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #e2e2e5;
+  background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
+
+  .code {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .num {
+      width: 98px;
+      height: 39px;
+      line-height: 39px;
+      cursor: pointer;
+    }
+
+    input {
+      width: 120px !important;
+    }
+
+    input:focus {
+      width: 120px !important;
+    }
+  }
+
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 350px;
+    height: 450px;
+    border-radius: 20px;
+    background-color: #3c80ce;
+    /* 盒子阴影 */
+    box-shadow: 5px 5px 25px 8px rgba(33, 45, 58, 0.3);
+    overflow: hidden;
+    position: relative;
+    z-index: 2200;
+    text-align: center;
+    color: #fff;
+
+    h1 {
+      font-size: 40px;
+      font-weight: 100;
+      letter-spacing: 2px;
+      margin-bottom: 15px;
+      /* 过渡效果 */
+      transition: 1s ease-in-out;
+      z-index: 2300;
+    }
+
+    .form {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      position: relative;
+      z-index: 2;
+      opacity: 1;
+      /* 不透明度改变时的过渡效果 */
+      transition: opacity 0.5s;
+
+      input {
+        outline: none;
+        border: 1px solid rgba(255, 255, 255, 0.4);
+        background-color: rgba(255, 255, 255, 0.2);
+        width: 218px;
+        padding: 10px 15px;
+        border-radius: 3px;
+        margin: 10px auto 10px auto;
+        text-align: center;
+        color: #fff;
+        font-size: 15px;
+        transition: 0.25s;
+      }
+
+      input::placeholder {
+        color: #fff;
+        font-size: 14px;
+        font-weight: 300;
+      }
+
+      input:hover {
+        background-color: rgba(255, 255, 255, 0.4);
+      }
+
+      input:focus {
+        background-color: #fff;
+        width: 270px;
+        color: #ff719a;
+      }
+    }
+
+    .btn-login {
+      outline: none;
+      background-color: #fff;
+      color: #ff719a;
+      border: none;
+      width: 250px;
+      padding: 10px 15px;
+      border-radius: 3px;
+      font-size: 15px;
+      cursor: pointer;
+      transition: 0.25s;
+      margin-top: 10px;
+    }
+
+    .btn-login:hover {
+      background-color: #f5f7f9;
+    }
+  }
 }
 
+/* 背景方块 */
+.bg-squares {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 1;
+  overflow: hidden;
+}
+
+.bg-squares li {
+  width: 40px;
+  height: 40px;
+  background-color: rgba(255, 255, 255, 0.15);
+  position: absolute;
+  bottom: -160px;
+  border-radius: 15px;
+  /* 执行动画：动画名 时长 线性 无限次播放 */
+  animation: square 20s linear infinite;
+}
+
+/* 为每一个方块设置不同的位置、大小、动画延迟时间、动画时长、背景色 */
+.bg-squares li:nth-child(1) {
+  left: 10%;
+}
+
+.bg-squares li:nth-child(2) {
+  left: 20%;
+  width: 80px;
+  height: 80px;
+  /* 动画延迟时间 */
+  animation-delay: 2s;
+  /* 动画时长 */
+  animation-duration: 17s;
+}
+
+.bg-squares li:nth-child(3) {
+  left: 25%;
+  animation-delay: 4s;
+}
+
+.bg-squares li:nth-child(4) {
+  left: 40%;
+  width: 60px;
+  height: 60px;
+  background-color: rgba(255, 255, 255, 0.25);
+  animation-duration: 22s;
+}
+
+.bg-squares li:nth-child(5) {
+  left: 70%;
+}
+
+.bg-squares li:nth-child(6) {
+  left: 80%;
+  width: 120px;
+  height: 120px;
+  background-color: rgba(255, 255, 255, 0.2);
+  animation-delay: 3s;
+}
+
+.bg-squares li:nth-child(7) {
+  left: 32%;
+  width: 160px;
+  height: 160px;
+  animation-delay: 7s;
+}
+
+.bg-squares li:nth-child(8) {
+  left: 55%;
+  width: 20px;
+  height: 20px;
+  animation-delay: 15s;
+  animation-duration: 40s;
+}
+
+.bg-squares li:nth-child(9) {
+  left: 25%;
+  width: 10px;
+  height: 10px;
+  background-color: rgba(255, 255, 255, 0.3);
+  animation-delay: 2s;
+  animation-duration: 40s;
+}
+
+.bg-squares li:nth-child(10) {
+  left: 90%;
+  width: 160px;
+  height: 160px;
+  animation-delay: 11s;
+}
+
+.container.success h1 {
+  transform: translateY(100px);
+}
+
+.container.success .form {
+  opacity: 0;
+  visibility: hidden;
+}
+
+.container.fail h1 {
+  transform: translateY(0);
+}
+
+.container.fail .form {
+  opacity: 1;
+  visibility: visible;
+}
+
+/* 定义动画 */
+@keyframes square {
+  0% {
+    transform: translateY(0);
+    //opacity: 0.15;
+  }
+
+  100% {
+    transform: translateY(-120vh) rotate(600deg);
+    opacity: 0.1;
+  }
+}
+</style>
+
+<style lang="scss">
+/* 设置鼠标进入的样式 */
 .container {
-  width: 640px;
-  height: 320px;
-  display: flex;
-  margin-top: -4%;
+  .in {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+    width: 0;
+    height: 0;
+    border-radius: 50%;
+    background: #cf455f;
+    transform: translate(-50%, -50%);
+    /* 使用in动画，持续0.5秒，缓出的时间函数，停留在最后一帧 */
+    animation: in 0.5s ease-out forwards;
+  }
+
+  /* 设置鼠标离开的样式 */
+  .out {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+    width: 1200px;
+    height: 1200px;
+    border-radius: 50%;
+    background: #cf455f;
+    transform: translate(-50%, -50%);
+    /* 使用out动画，持续0.5秒，缓出的时间函数，停留在最后一帧 */
+    animation: out 0.5s ease-out forwards;
+  }
 }
 
-.left {
-  width: 50%;
-  height: calc(100% - 40px);
-  background-color: #fff;
-  position: relative;
-  top: 20px;
+/* 动画 */
+/* 设置鼠标进入时，元素的动画 */
+@keyframes in {
+
+  /* 初始关键帧 */
+  0% {
+    width: 0;
+    height: 0;
+  }
+
+  /* 结束关键帧 */
+  100% {
+    width: 1200px;
+    height: 1200px;
+  }
 }
 
-.left h1 {
-  color: #222;
-  font-size: 50px;
-  font-weight: bold;
-  text-transform: uppercase;
-  letter-spacing: 4px;
-  margin: 55px 40px 40px 40px;
-}
+/* 设置鼠标离开时，元素的动画 */
+@keyframes out {
 
-.left p {
-  color: #999;
-  font-size: 14px;
-  margin: 40px;
-  line-height: 22px;
-}
+  /* 初始关键帧 */
+  0% {
+    width: 1200px;
+    height: 1200px;
+  }
 
-.right {
-  width: 50%;
-  height: 100%;
-  background-color: #474a59;
-  color: #f1f1f1;
-  position: relative;
-  box-shadow: 0 0 40px 16px rgba(0, 0, 0, 0.2);
-}
-
-.right svg {
-  position: absolute;
-  width: 320px;
-  left: 3px;
-  top: 23px;
-}
-
-.right path {
-  /* 取消填充 */
-  fill: none;
-  /* 定义一条线,引用定义好的线性渐变 */
-  stroke: url(#linearGradient1);
-  /* 线的厚度 */
-  stroke-width: 4;
-  /* 设置虚线:虚线长度 间距 */
-  stroke-dasharray: 240 1386;
-}
-
-.form {
-  margin: 40px;
-  position: absolute;
-}
-
-.account {
-  margin-top: 35px !important;
-}
-
-.form label {
-  color: #c2c2c2;
-  display: block;
-  font-size: 14px;
-  margin-top: 20px;
-  margin-bottom: 5px;
-  text-align: left;
-}
-
-.password {
-  margin-top: 15px !important;
-}
-
-.form input {
-  width: 100%;
-  height: 35px;
-  line-height: 35px;
-  font-size: 20px;
-  color: #f2f2f2;
-  background-color: transparent;
-  border: none;
-  outline: none;
-  text-indent: 2px;
-}
-
-.form button {
-  width: 100%;
-  height: 30px;
-  color: #d0d0d0;
-  font-size: 18px;
-  background-color: transparent;
-  border: none;
-  margin-top: 27px;
-  cursor: pointer;
-  outline: none;
+  /* 结束关键帧 */
+  100% {
+    width: 0;
+    height: 0;
+  }
 }
 </style>
